fb5305e8c762b6da706906cef6bb2f523177ae12
Andrew Yurick <39890868+andrewyurick@users.noreply.github.com>
2022-04-28 19:29:17 -0400

    Update project.sol

--- project.sol
+++ project.sol
@@ -1,3 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 // Sol Test
+contract HelloWorld {
+
+    // State variables
+    string helloMsg = "Hello, world!";
+
+    // On creation...
+    constructor () {
+        // Do nothing for now, because we don't need to
+    }
+
+    // Print hello world
+    function sayHello() public view returns (string memory) {
+        return helloMsg;
+    }
+}
